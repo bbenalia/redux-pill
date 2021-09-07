1e6387ab6796925fcb94ffd49a277b6b31e30e25
Nachomontoya <montoyanacho@gmail.com>
2021-09-07 13:59:49 +0200

    grid on dashboard created

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import RadioButton from "../../components/RadioButton";
 import SelectButton from "../../components/SelectButton";
+import SearchBar from "../../components/SearchBar";
 import withLayout from "../../hoc/withLayout";
 
 function Dashboard() {
@@ -11,24 +12,78 @@ function Dashboard() {
   return (
     <>
       <div className="container mt-5">
-        <RadioButton />
-      </div>
-      <div className="container">
-        <SelectButton unselected value={0} onClick={toggleSelect}>
-          0 (studio flat)
-        </SelectButton>
-        <SelectButton unselected value={1} onClick={toggleSelect}>
-          1
-        </SelectButton>
-        <SelectButton unselected value={2} onClick={toggleSelect}>
-          2
-        </SelectButton>
-        <SelectButton unselected value={3} onClick={toggleSelect}>
-          3
-        </SelectButton>
-        <SelectButton unselected value={4} onClick={toggleSelect}>
-          4 or +
-        </SelectButton>
+        <div className="row">
+          <div className="col">
+            <SearchBar />
+          </div>
+          <div className="col text-end align-items-center">
+            <img
+              src="https://github.com/mdo.png"
+              alt="mdo"
+              width="32"
+              height="32"
+              className="rounded-circle d-inline"
+            />
+            <h6 className="d-inline ms-2">username</h6>
+          </div>
+        </div>
+        <div className="container mt-5 shadow-sm rounded p-3">
+          <div className="row">
+            <div className="col-3">
+              <h6>Type of home</h6>
+              <RadioButton />
+            </div>
+            <div className="col-3">
+              <h6>Bedrooms</h6>
+              <SelectButton unselected value={0} onClick={toggleSelect}>
+                0 (studio flat)
+              </SelectButton>
+              <SelectButton unselected value={1} onClick={toggleSelect}>
+                1
+              </SelectButton>
+              <SelectButton unselected value={2} onClick={toggleSelect}>
+                2
+              </SelectButton>
+              <SelectButton unselected value={3} onClick={toggleSelect}>
+                3
+              </SelectButton>
+              <SelectButton unselected value={4} onClick={toggleSelect}>
+                4 or +
+              </SelectButton>
+            </div>
+            <div className="col-3">
+              <h6>Bathrooms</h6>
+              <SelectButton unselected value={1} onClick={toggleSelect}>
+                1
+              </SelectButton>
+              <SelectButton unselected value={2} onClick={toggleSelect}>
+                2
+              </SelectButton>
+              <SelectButton unselected value={3} onClick={toggleSelect}>
+                3 or +
+              </SelectButton>
+            </div>
+            <div className="col-3">
+              <h6>Equipment</h6>
+            </div>
+          </div>
+          <div className="row mt-4">
+            <div className="col-3">
+              <h6>Condition</h6>
+            </div>
+            <div className="col-3">
+              <h6>Price Range</h6>
+            </div>
+            <div className="col-3">
+              <h6>Publication Date</h6>
+            </div>
+            <div className="col-3">
+              <h6>More Filters</h6>
+            </div>
+          </div>
+        </div>
+
+        <div className="container mt-5 shadow-sm rounded p-3">Image</div>
       </div>
     </>
   );
