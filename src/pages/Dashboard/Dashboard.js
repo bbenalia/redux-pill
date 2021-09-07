0169fc85d1cb39357aa4e427dd781392b558b0a4
Nachomontoya <montoyanacho@gmail.com>
2021-09-07 13:13:54 +0200

    select button created

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,12 +1,36 @@
 import React from "react";
 import RadioButton from "../../components/RadioButton";
+import SelectButton from "../../components/SelectButton";
 import withLayout from "../../hoc/withLayout";
 
 function Dashboard() {
+  const toggleSelect = (event) => {
+    event.target.classList.toggle("unselected");
+  };
+
   return (
-    <div className="container mt-5">
-      <RadioButton />
-    </div>
+    <>
+      <div className="container mt-5">
+        <RadioButton />
+      </div>
+      <div className="container">
+        <SelectButton unselected value={0} onClick={toggleSelect}>
+          0 (studio flat)
+        </SelectButton>
+        <SelectButton unselected value={1} onClick={toggleSelect}>
+          1
+        </SelectButton>
+        <SelectButton unselected value={2} onClick={toggleSelect}>
+          2
+        </SelectButton>
+        <SelectButton unselected value={3} onClick={toggleSelect}>
+          3
+        </SelectButton>
+        <SelectButton unselected value={4} onClick={toggleSelect}>
+          4 or +
+        </SelectButton>
+      </div>
+    </>
   );
 }
 
