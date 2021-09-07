01386e4bdb8a73625e9c0d62f283d4c5d5c4f733
Nachomontoya <montoyanacho@gmail.com>
2021-09-07 10:35:27 +0200

    first styles

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,26 @@
 import React from "react";
 import withLayout from "../../hoc/withLayout";
 
+import SearchBar from "../../components/SearchBar";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <div className="mt-5 row">
+        <div className="col-6 pe-2">
+          <h3 className="mb-4">Lorem fistrum a gramenawer caballo blanco</h3>
+          <p>
+            Sexuarl pupita no te digo trigo por no llamarte Rodrigor no te digo
+            trigo por no llamarte Rodrigor tiene musho peligro a wan me cago en
+            tus muelas.
+          </p>
+        </div>
+        <div className="col-6">
+          <SearchBar />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default withLayout(Home);
