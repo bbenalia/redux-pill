549aacbf0ccf085be2b8b502095dab6cf0309cb3
Brahim Benalia <65949632+bbenalia@users.noreply.github.com>
2021-09-07 10:33:26 +0200

    Feat f/2/header and basic router (#5)
    * Add basic routing
    
    * Feat: add hoc header
    
    * deleted package lock

--- src/hoc/withLayout.js
+++ src/hoc/withLayout.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import AppHeader from "../components/Header";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Main from "../components/Main";
 
@@ -16,13 +16,13 @@ function withLayout(WrappedComponent) {
 
   function WrapperComponent({ ...props }) {
     return (
-      <>
-        <AppHeader />
+      <div className="container">
+        <Header />
         <Main>
           <WrappedComponent {...props} />
         </Main>
         <Footer />
-      </>
+      </div>
     );
   }
 
