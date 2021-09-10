96653f0a4d34ade9a74d49d598898a2cab8f4737
Brahim <brahimcasas@hotmail.es>
2021-09-10 09:41:00 +0200

    Fix: header navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import { HOME, DASHBOARD } from "../../constants/routes";
 
@@ -10,21 +10,32 @@ export default function Header() {
     <header className="container">
       <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container-fluid">
-          <Link className="navbar-brand" to={HOME}>
+          <NavLink className="navbar-brand" to={HOME}>
             Redux Project
-          </Link>
+          </NavLink>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to={HOME}>
+                <NavLink
+                  className="nav-link"
+                  activeClassName="active"
+                  exact
+                  to={HOME}
+                >
                   Home
-                </Link>
+                </NavLink>
               </li>
+
               <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to={DASHBOARD}>
+                <NavLink
+                  className="nav-link"
+                  activeClassName="active"
+                  exact
+                  to={DASHBOARD}
+                >
                   Dashboard
-                </Link>
+                </NavLink>
               </li>
             </ul>
 
